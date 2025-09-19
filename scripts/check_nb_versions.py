9ee6183d10336d2adc09446c191fe649038ab4a9
asoplata <asoplata@users.noreply.github.com>
2025-09-19 15:01:19 +0000

    deploy: 022fd51c0315ea3621dd5e75a2f88a8ce62cbf93

--- scripts/check_nb_versions.py
+++ scripts/check_nb_versions.py
@@ -1,7 +1,47 @@
 import sys
 
+from scripts.logger_setup import setup_logger
 
-def check_version():
+
+def check_version(enable_debug=True):
+    """
+    Return True if all notebooks are run on the same version of hnn_core,
+    else False
+
+    This function performs the following steps:
+    1. Get notebooks to skip from 'notebooks_to_skip.json'.
+    2. Get the json output files for notebooks and extract the version used
+       to run the notebook
+    3. Get the latest version of hnn-core from pypi
+    4. Check if all notebooks are run on the same version of hnn-core
+        - If not, warn user
+    5. If yes, compare the version used for execution with the latest version from pypi
+        - If not, warn user
+    7. Print all notebooks and versions used for execution in the format:
+        {
+            'plot_simulate_evoked.json': {'hnn_version': '0.4.2'},
+            'plot_simulate_gamma.json': {'hnn_version': '0.4.1'},
+            ...
+        }
+
+    Inputs
+    ------
+    debug : bool
+        If True, enables debug logging to print verbose information about
+        the process
+
+    Returns
+    -------
+    bool
+        True if all executed notebooks were run with the latest
+        version of hnn-core, else False
+    """
+
+    logger = setup_logger(__name__, enable_debug=enable_debug)
+
+    logger.debug(
+        'DEV',
+    )
     import json
     import os
 
@@ -27,6 +67,11 @@ def check_version():
 
     # get names of notebooks to skip
     notebooks_to_skip = notebooks_to_skip["skip_execution"]
+    logger.debug(
+        '\n',
+        'Notebooks to skip:\n',
+        notebooks_to_skip,
+    )
 
     # get json filenames for executed notebooks only
     json_fnames = [
@@ -34,6 +79,11 @@ def check_version():
         for notebook in notebook_hashes.keys()
         if notebook not in notebooks_to_skip
     ]
+    logger.debug(
+        '\n',
+        'Notebooks to run:\n',
+        json_fnames,
+    )
 
     # get filepaths for each filename in json_fnames
     json_fpaths = []
@@ -49,13 +99,20 @@ def check_version():
 
     # get the value of the "hnn_version" key from each json
     notebook_versions = []
+    execution_statuses = dict()
     for file in json_fpaths:
+        file_key = file.split(os.sep)[-1]
+        if file_key not in execution_statuses.keys():
+            execution_statuses[file_key] = dict()
         with open(file, "r") as f:
             contents = json.load(f)
             if "hnn_version" in contents:
                 notebook_versions.append(contents["hnn_version"])
+                execution_statuses[file_key]['hnn_version'] = contents["hnn_version"]
             else:
                 print(f"Version key not found in {file}")
+            # if "master_commit" in contents:
+                # execution_statuses[file]['master_commit'] =
 
     # unique versions for executed notebooks
     notebook_versions = list(set(notebook_versions))
@@ -75,11 +132,20 @@ def check_version():
         )
     else:
         print(
-            f"Latest version of hnn-core: {latest}",
-            f"Versions of hnn-core used in executed notebooks: {notebook_versions}",
+            f"\nLatest version of hnn-core: {latest}",
+            f"\nVersions of hnn-core used in executed notebooks: {notebook_versions}",
             "Notebooks should be re-executed using the latest version",
         )
 
+    logger.debug(
+        '\nExecution statuses:',
+        '\n',
+        execution_statuses
+    )
+
+    print(
+        '\nReturn:'
+    )
     return latest_version_check
 
 
---
+++ scripts/logger_setup.py
@@ -0,0 +1,40 @@
+import logging
+
+
+def setup_logger(name, enable_debug=False,):
+    # set root logger to warning to stop external DEBUG logs
+    logging.getLogger().setLevel(logging.WARNING)
+
+    # create logger
+    logger = logging.getLogger(name)
+
+    # remove any existing handlers
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+
+    if enable_debug:
+        logger.setLevel(logging.DEBUG)
+
+    # disable propagation to root logger
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    # set up formatted
+    formatter = logging.Formatter(
+        '# DEBUG: %(name)s' +  # DEBUG: module.submodule
+        '\n# ' + '-' * 30 +    # -----------------------
+        '\n%(message)s\n'      # output message contents
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    orig_debug = logger.debug
+
+    # wrapper to allow multiple arguments to debug function
+    def debug(*args):
+        message = " ".join(str(arg) for arg in args)
+        orig_debug(message)
+
+    logger.debug = debug
+
+    return logger
